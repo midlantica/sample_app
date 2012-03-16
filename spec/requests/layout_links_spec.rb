8de9486968610b71745d37904f3dd80043287b8e
Drew Harper <druharper@gmail.com>
2012-03-16 00:32:35 -0400

    finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,4 +21,11 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+  it "should have a Sign Up page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign Up")
+  end
+
+
+
 end
