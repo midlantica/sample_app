babcc087bc5a4184ade5382df8b8191fe012995b
Drew Harper <druharper@gmail.com>
2012-03-16 00:35:54 -0400

    xxxx

--- Gemfile
+++ Gemfile
@@ -1,26 +1,13 @@
 source 'http://rubygems.org'
 
-gem 'rails'
+gem 'rails', '3.2.1'
 gem 'sqlite3'
 
 group :development do
-	gem 'rspec-rails', '2.6.1'
-	# gem 'rspec'
+	gem 'rspec-rails', '2.0.1'
 end
-
 
 group :test do
-	gem 'rspec-rails', '2.6.1'
+	gem 'rspec', '2.0.1'
 	gem 'webrat', '0.7.1'
-	gem 'autotest', '4.4.6'
-	gem 'autotest-rails-pure', '4.1.2'
-	# gem 'autotest-fsevent',  '0.2.4'
-	gem 'autotest-growl', '0.2.6'
-	
 end
-
-
-
-
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
-  match '/signup',  :to => 'users#new'
-
-  match '/contact', :to => 'pages#contact'
-  match '/about',   :to => 'pages#about'
-  match '/help',    :to => 'pages#help'
-  root :to => 'pages#home'
-
-  
-
-  # OLD ROUTES
-  # get "pages/home"
-  # get "pages/contact"
-  # get "pages/about"
-  # get "pages/help"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -65,11 +48,11 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'pages#home'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  # match ':controller(/:action(/:id))(.:format)'
 end
