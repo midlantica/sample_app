34d765597a574484dee6d076139ba67e5950bbc3
Drew Harper <druharper@gmail.com>
2012-03-16 21:15:17 -0400

    Finishwed layout and routes 2

--- Gemfile
+++ Gemfile
@@ -1,13 +1,26 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.2.1'
+gem 'rails'
 gem 'sqlite3'
 
 group :development do
-	gem 'rspec-rails', '2.0.1'
+	gem 'rspec-rails', '2.6.1'
+	# gem 'rspec'
 end
 
+
 group :test do
-	gem 'rspec', '2.0.1'
+	gem 'rspec-rails', '2.6.1'
 	gem 'webrat', '0.7.1'
+	gem 'autotest', '4.4.6'
+	gem 'autotest-rails-pure', '4.1.2'
+	# gem 'autotest-fsevent',  '0.2.4'
+	gem 'autotest-growl', '0.2.6'
+	
 end
+
+
+
+
+
+
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '956a8c260047e9231a639c7f4ee0e238e68452c30280f7eeec5b258542c34ebd5d54e28ce17020425115d1ebb53b286db7a91a9f1790a3a713de376f9b51cf2e'
+SampleApp::Application.config.secret_token = '261df1bf3a402af6747234ae97f1fcc2bfdec3a1fd4c5019b04bda1df387cce03e48d8e8d4c57489eb82d5f0803286c21d9328c219e75ddeb809a5365fdbc5df'
